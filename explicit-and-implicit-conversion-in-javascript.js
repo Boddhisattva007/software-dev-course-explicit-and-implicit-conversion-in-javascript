792b042c1d344290c2973c43b1114b3f0c1296f0
Brian Taylor <brtaylor0010@gmail.com>
2026-03-12 16:12:38 -0500

    Completed Debug Challenge & Provided My Own Code Examples For Both Implicit and Explicit Type Conversions, Including Edge Case

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,42 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
-console.log("The result is: " + result);
+let result = Number("5") - 2; // type converted the string "5" to a number
+console.log("The result is: " + result); // now it is clear a number is being subtracted from another number
 
-let isValid = Boolean("false");
+let booleanValue = "false"; //assigned string value of "false" to the variable booleanValue because "false" is truthy
+let isValid = Boolean(booleanValue === "true"); // checking if string "false" equals string "true" which returns false
 if (isValid) {
-    console.log("This is valid!");
+    console.log("This is valid!"); // nothing prints as a result
 }
 
-let age = "25";
-let totalAge = age + 5;
+let age = Number("25"); // type converted the string "25" to a number
+let totalAge = age + 5; // addition can now be performed between two numbers
 console.log("Total Age: " + totalAge);
+
+//// OWN CODE EXAMPLES ////
+
+//___IMPLICIT TYPE CONVERSION___//
+
+console.log("")
+
+console.log("Implicit Type Conversion Example:")
+let answer = "And the answer is: Number " + 7; // the number 7 is being converted to a string
+console.log(answer);
+
+console.log("After Fix:")
+let answer2 = "And the answer is: Number " + String(7); // type converted number to a string first
+console.log(answer2);
+
+//___EXPLICIT TYPE CONVERSION___//
+
+console.log("")
+
+console.log("Explicit Type Conversion Example:")
+let userInput = null; 
+if (Boolean(userInput) === false) {                // converting userInput into a Boolean and checking if true/false
+  console.log("Please Enter Username: ")           // null === false so evaluates to true and statment prints
+}
+
+console.log("")
+
